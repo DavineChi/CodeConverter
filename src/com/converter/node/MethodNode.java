7230176bc3102bca2b14831761645d3650dbfc37
Shannon Fisher <davine_chi@hotmail.com>
2021-06-02 10:23:59 -0500

    Add additional fields and getters

--- src/com/converter/node/MethodNode.java
+++ src/com/converter/node/MethodNode.java
@@ -2,17 +2,42 @@ package com.converter.node;
 
 public class MethodNode extends ASTNode {
 	
+	private String nodeAccessModifier;
+	private String nodeCallType;
+	private String nodeReturnType;
 	private String nodeName;
 	private ParameterNode[] nodeParameters;
 	private ASTNode nodeBody;
 	
-	public MethodNode(String name, String[] variables, ASTNode body) {
+	public MethodNode(String accessModifier,
+                      String callType,
+                      String returnType,
+                      String name,
                       ParameterNode[] parameters,
+                      ASTNode body) {
 		
 		this.nodeType = NodeType.METHOD;
+		this.nodeAccessModifier = accessModifier;
+		this.nodeCallType = callType;
+		this.nodeReturnType = returnType;
 		this.nodeName = name;
 		this.nodeParameters = parameters;
 		this.nodeBody = body;
+	}
+	
+	public String getAccessModifier() {
+		
+		return nodeAccessModifier;
+	}
+	
+	public String getCallType() {
+		
+		return nodeCallType;
+	}
+	
+	public String getReturnType() {
+		
+		return nodeReturnType;
 	}
 	
 	public String getName() {
