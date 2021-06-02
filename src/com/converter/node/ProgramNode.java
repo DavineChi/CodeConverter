06996e3e20485c7561a587d7a5fbdd4da67c82d7
Shannon Fisher <davine_chi@hotmail.com>
2021-06-02 10:20:45 -0500

    Add size and capacity fields

--- src/com/converter/node/ProgramNode.java
+++ src/com/converter/node/ProgramNode.java
@@ -5,12 +5,15 @@ public class ProgramNode extends ASTNode {
 	private static final int INITIAL_CAPACITY = 10;
 	
 	private ASTNode[] nodeNodes;
+	private int capacity;
+	private int size;
 	
 	public ProgramNode() {
 		
 		this.nodeType = NodeType.PROGRAM;
-		this.nodeProgram = program;
+		this.capacity = INITIAL_CAPACITY;
 		this.nodeNodes = new ASTNode[INITIAL_CAPACITY];
+		this.size = 0;
 	}
 	
 	public ProgramNode(ASTNode node) {
