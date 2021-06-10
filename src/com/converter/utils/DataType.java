5f8877d9b4b92d058c92d458dbfa8c9cf71fd69b
Shannon Fisher <davine_chi@hotmail.com>
2021-06-10 17:48:17 -0500

    Add new DataType class

---
+++ src/com/converter/utils/DataType.java
@@ -0,0 +1,22 @@
+package com.converter.utils;
+
+public class DataType {
+	
+	public static final String[] LIST = {
+		
+		"Byte",
+		"Boolean",
+		"Integer",
+		"Long",
+		"Single",
+		"Double",
+		"Currency",
+		"Decimal",
+		"Date",
+		"Object",
+		"String",
+		"Variant",
+		"Connection",
+		"Recordset"
+	};
+}
