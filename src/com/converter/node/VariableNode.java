3fdd9b07ea186c98f316d488974f1513d11169df
Shannon Fisher <davine_chi@hotmail.com>
2021-07-01 23:46:23 -0500

    Remove nodeIsConstant field, methods, constructor

--- src/com/converter/node/VariableNode.java
+++ src/com/converter/node/VariableNode.java
@@ -7,20 +7,13 @@ public class VariableNode extends ASTNode {
 	private String nodeAccessModifier;
 	private String nodeDataType;
 	private String nodeName;
-	private boolean nodeIsConstant;
 	
 	public VariableNode(String accessModifier, String dataType, String name) {
-		
-		this(accessModifier, dataType, name, false);
-	}
-	
-	public VariableNode(String accessModifier, String dataType, String name, boolean isConstant) {
 		
 		this.nodeType = NodeType.VARIABLE;
 		this.nodeAccessModifier = accessModifier;
 		this.nodeDataType = dataType;
 		this.nodeName = name;
-		this.nodeIsConstant = isConstant;
 	}
 	
 	public String getAccessModifier() {
@@ -36,11 +29,6 @@ public class VariableNode extends ASTNode {
 	public String getName() {
 		
 		return nodeName;
-	}
-	
-	public boolean isConstant() {
-		
-		return nodeIsConstant;
 	}
 	
 	@Override
