6428d743175f805b6a67003764f5ec46364d977e
Shannon Fisher <davine_chi@hotmail.com>
2021-07-03 21:59:09 -0500

    Reduce initial capacity for INodeCollections

--- src/com/converter/node/INodeCollection.java
+++ src/com/converter/node/INodeCollection.java
@@ -2,7 +2,7 @@ package com.converter.node;
 
 public interface INodeCollection {
 	
-	static final int INITIAL_CAPACITY = 10;
+	static final int INITIAL_CAPACITY = 5;
 	
 	abstract void addNode(ASTNode node);
 }
